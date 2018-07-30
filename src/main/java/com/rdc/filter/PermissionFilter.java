7c9ea22400c8758717b357fb0abcb76c3bc2a38b
张瑞钧 <3117002462@mail2.gdut.edu.cn>
2018-07-30 20:07:59 +0800

    'delete.idea'

--- src/main/java/com/rdc/filter/PermissionFilter.java
+++ src/main/java/com/rdc/filter/PermissionFilter.java
@@ -1,10 +1,12 @@
 package com.rdc.filter;
 
+import com.rdc.entity.User;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 public class PermissionFilter implements HandlerInterceptor {
 
