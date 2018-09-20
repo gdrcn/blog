a55554549b66a86e7f9d0e5a0a90fd35accf340d
gdrcn <1062081835@qq.com>
2018-09-20 22:24:03 +0800

    v2.2

--- src/main/java/com/rdc/filter/PermissionFilter.java
+++ src/main/java/com/rdc/filter/PermissionFilter.java
@@ -1,21 +1,19 @@
 package com.rdc.filter;
 
-import com.rdc.entity.User;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 public class PermissionFilter implements HandlerInterceptor {
 
 	@Override
     public boolean preHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o) {
-        HttpSession session = httpServletRequest.getSession();
-        User user = new User();
-        user.setId(1);
-        session.setAttribute("user", user);
+//        HttpSession session = httpServletRequest.getSession();
+//        User user = new User();
+//        user.setId(1);
+//        session.setAttribute("user", user);
 		return true;
 	}
 
--- test/com/rdc/bean/UserBeanTest.java
+++ test/com/rdc/bean/UserBeanTest.java
@@ -1,8 +1,12 @@
 package com.rdc.bean;
 
+import org.junit.Before;
 import org.junit.Test;
 
+import java.util.*;
 import java.util.stream.Stream;
+
+import static org.junit.Assert.assertSame;
 
 public class UserBeanTest {
 
@@ -28,6 +32,11 @@ public class UserBeanTest {
 
     @Test
     public void test2() {
+        List a = new ArrayList();
+        List b = new LinkedList();
+        Map c = new HashMap<>();
+        Map d = new TreeMap<>();
+        System.out.println((null != null) ? 0 : 1);
 //        int sum = widgets.stream()
 //                .filter(w -> w.getColor() == RED)
 //                .mapToInt(w -> w.getWeight())
@@ -41,4 +50,65 @@ public class UserBeanTest {
 //        System.out.println(concat);
         Stream.iterate(0, n -> n + 3).limit(1000).forEach(x -> System.out.print(x + " "));
     }
+
+    @Test
+    public void test3() {
+        String name = "abner chai";
+        //String name = null;
+        assert (name == null) : "变量name为空null";
+//        System.out.println(name);
+    }
+
+    @Test
+    public void test4() {
+        int i = 0;
+        String greetings[] = {" Hello world !", " Hello World !! ",
+                " HELLO WORLD !!!"};
+        while (i < 5) {
+            try {
+                i++;
+            } finally {
+                System.out.println("--------------------------");
+            }
+//            assertEquals(60, result, 0);
+        }
+    }
+
+    @Test
+    public void test5() {
+        Object a = 3;
+        Object b = 3;
+        assertSame("不相同", a, b);
+    }
+
+    @Test
+    public void test7() {
+        String a = "123";
+        String b = "12";
+        assertSame("1", a, b);
+    }
+
+//    @Ignore
+//    @Test(timeout = 30)
+//    public void test8(){
+//        for(int i = 0; i < 10000; i++){
+//            System.out.println(i);
+//        }
+//    }
+
+    List<String> values;
+
+    @Before
+    public void setUpList() {
+        values = new ArrayList<String>();
+        values.add("x");
+        values.add("y");
+        values.add("z");
+    }
+
+    @Test
+    public void test10() {
+
+    }
+
 }
